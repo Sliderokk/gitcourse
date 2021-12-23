f684909f497cb1f1f04c46cd4139f36d4dd766ec
Sliderokk <kuksov.sergey.victorovich@gmail.com>
2021-12-23 17:22:31 +0200

    Rename res

---
+++ res.php
@@ -0,0 +1,25 @@
+<p>Ваш результат:</p>
+
+
+<?php
+/*
+echo $_POST;
+$answer3 = $_POST['$answer3'];
+echo $answer3;
+*/
+?>
+
+<?php
+
+
+function slid(){
+    $b = 20;
+    $a = 10.234;
+
+    $c = round($a);
+echo $c * $b;
+}
+
+slid();
+
+?>
